139c053bb32369d380645325f9e0a8e42d44a0b7
Zakariyae EL MAATAOUI <z.elmaataoui@labelvie.ma>
2022-07-19 09:11:43 +0100

    3em Commit

---
+++ automate_versement/phase_2/send_versement.py
@@ -0,0 +1,58 @@
+
+import locale
+import datetime
+from datetime import date
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+
+
+########################################################################################################## SEND EMAIL
+yesterday = date.today() - datetime.timedelta(days=1)
+yesterday = str(yesterday)
+#yesterday = "2022-06-25"
+
+
+def send():
+    fromaddr = "z.elmaataoui@labelvie.ma"
+    toaddr = "h.ramzi@labelvie.ma", "k.naciri@labelvie.ma"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = ', '.join(toaddr)
+    msg['Subject'] = "RAPPORT LIVRAISON - SQUAD ECOMMERCE : {}".format(yesterday)
+    body = """
+Bonjour, 
+
+Veuillez trouver ci-joint le rapport d'activité transactionnelle de Bringo by Carrefour du {}.
+
+Nb : Ce mail est envoyé automatiquement.
+
+Bonne réception,
+Zakariyae 
+""".format(yesterday)
+    msg.attach(MIMEText(body, 'plain'))
+    filename = "Livraison_rapport - {}.xlsx".format(yesterday)
+    attachment = open("C:/Users/elmaa/OneDrive/Bureau/rapport_livraison/Livraison_rapport.xlsx", "rb")
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, "Triwakof2022")
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
+send()
+
+def fin_3():
+    for i in range(10):
+        print(".................................................. Done {}".format(i))
+    print(".................................................. Done")
+
+fin_3()
